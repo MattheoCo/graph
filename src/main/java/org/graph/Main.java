31ef054ddab1f99938ef46672610c7116692dd60
mattheo <mattheocognet@gmail.com>
2024-01-30 10:12:20 +0100

    premier test fonction création et affichage graph

---
+++ src/main/java/org/graph/Main.java
@@ -0,0 +1,18 @@
+package org.graph;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Edge> edge = Arrays.asList(
+                new Edge(1,0, 23),
+                new Edge(1, 2, 45),
+                new Edge(2, 0, 12),
+                new Edge(0, 3, 32)
+
+        );
+    Graph graph = new Graph(edge);
+    Graph.afficheGraph(graph);
+    }
+}
